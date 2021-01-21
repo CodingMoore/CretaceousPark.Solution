085699a0485fcdf8e01269717d46ea93d07178de
Randel M <Randel.C.Moore@gmail.com>
2021-01-20 16:23:04 -0800

    add Edit and delete functionality
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- CretaceousClient/Controllers/AnimalsController.cs
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -15,6 +15,28 @@ namespace CretaceousClient.Controllers
         var allAnimals = Animal.GetAnimals();
         return View(allAnimals);
       }
-      
+
+      public IActionResult Details(int id)
+      {
+        var animalDetails = Animal.GetDetails(id);
+        return View(animalDetails);
+      }
+      public IActionResult Delete(int id)
+      {
+        Animal.Delete(id);
+        return RedirectToAction("Index");
+      }
+      public IActionResult Edit(int id)
+      {
+        var animal = Animal.GetDetails(id);
+        return View(animal);
+      }
+
+      [HttpPost]
+      public IActionResult Edit(Animal animal)
+      {
+        Animal.Put(animal);
+        return RedirectToAction("Details", new {id = animal.AnimalId});
+      }
     }
 }
